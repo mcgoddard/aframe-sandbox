43086c1e564f9fcb348f66a16e5e48cb0025b8c8
Mike Goddard <mikeygiom@gmail.com>
2017-09-14 00:36:35 +0100

    Some custom looking controls, not currently working correctly

--- index.js
+++ index.js
@@ -1,7 +1,37 @@
+var lastX = -1;
+var lastY = -1;
+
 window.onload = function() {
-    var camera = $("#camera");
-    var position = camera.attr("position");
-    console.log(position);
-    position.y = 1.6;
-    camera.attr("position", position.x + ', ' + position.y + ', ' + position.z);
+    $("body").mousemove(function(e) {
+        var newX = e.pageX;
+        var newY = e.pageY;
+        var reset = false;
+        if (lastX == -1) {
+            lastX = newX;
+            reset = true;
+        }
+        if (lastY == -1) {
+            lastY = newY;
+            reset = true;
+        }
+        if (reset) {
+            return;
+        }
+        else {
+            // Alter camera direction
+            // Get current direction
+            var camera = $("#camera");
+            var rotation = camera.attr("rotation");
+            // Calcuate new direction
+            var diffX = newX - lastX;
+            var diffY = newY - lastY;
+            var newPosX = (rotation.x + diffX);
+            var newPosY = (rotation.y + diffY);
+            // Set new direction
+            $("#camera").attr('rotation', newPosX+', '+newPosY+', 0');
+            // Update previous coordinates
+            lastX = newX;
+            lastY = newY;
+        }
+    });
 }
